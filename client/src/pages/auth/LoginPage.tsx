1be0f13556faa40095666fefe8235bea690a29a9
kaoutharrr <kkouaz04@gmail.com>
2026-04-25 00:34:17 +0100

    style: update LoginPage heading styles with Tailwind CSS

--- client/src/pages/auth/LoginPage.tsx
+++ client/src/pages/auth/LoginPage.tsx
@@ -1,7 +1,7 @@
 export default function LoginPage() {
   return (
     <div className="page">
-      <h1>LoginPage</h1>
+      <h1 className="text-red-400 text-4xl">LoginPage</h1>
       <p>TODO</p>
     </div>
   );
